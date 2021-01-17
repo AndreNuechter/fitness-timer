3dc92c9d1de4d6464a9b137e4b6a3a973e376d08
AN <andre_nuechter@hotmail.de>
2021-01-17 15:36:55 +0100

    Add basic cache invalidation

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,31 @@
 /* eslint-disable no-restricted-globals */
+
+const appName = 'fitness-timer';
+const appVersion = '3';
+const cacheName = `${appName}-v${appVersion}`;
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        (async () => {
+            const keys = await self.caches.keys();
+            keys.forEach((key) => {
+                if (key.includes(appName) && key !== cacheName) {
+                    self.caches.delete(key);
+                }
+            });
+        })()
+    );
+});
+
 self.addEventListener('fetch', (event) => {
-    event
-        .respondWith(
-            (async () => {
-                let response = await self.caches.match(event.request);
-                if (response) return response;
-                response = await fetch(event.request);
-                const cache = await self.caches.open('v2');
-                cache.put(event.request, response.clone());
-                return response;
-            })()
-        );
+    event.respondWith(
+        (async () => {
+            let response = await self.caches.match(event.request);
+            if (response) return response;
+            response = await fetch(event.request);
+            const cache = await self.caches.open(cacheName);
+            cache.put(event.request, response.clone());
+            return response;
+        })()
+    );
 });
